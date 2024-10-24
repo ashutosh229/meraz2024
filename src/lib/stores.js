b0bb78035550b9649e195a6f45f06c9a81df5c3f
ashutosh229 <akumarjha875@gmail.com>
2024-10-24 23:13:22 +0530

    added

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,4 +1,4 @@
-import { writable } from 'svelte/store';
+import { writable } from "svelte/store";
 
 export const sidebarOpen = writable(false);
 export const collapsingNavbar = writable(false);
--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,27 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static";
+import preprocess from "svelte-preprocess";
+import { mdsvex } from "mdsvex";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	},
-	preprocess: [vitePreprocess()]
-
+  extensions: [".svelte", ".md"],
+  kit: {
+    adapter: adapter(),
+    // hydrate the <div id="svelte"> element in src/app.html
+  },
+  preprocess: [
+    preprocess({
+      scss: {
+        includePaths: ["src/styles/"],
+      },
+    }),
+    mdsvex({
+      extensions: [".svelte", ".md"],
+      layout: {
+        event: "./src/lib/layouts/Event.svelte",
+      },
+    }),
+  ],
 };
 
 export default config;
